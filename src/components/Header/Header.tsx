d66eabfedffdf88b4420d65f5c33df051963c85e
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-03-06 16:58:18 -0300

    Menu alterado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import logo from './ID.png'
+import menuHamburguer from './hambuguer.png'
 import './header.css'
 
 export const Header = () => {
+  const setMenu = () => {
+    let menu = document.querySelector('nav');
+    menu?.classList.toggle("block")
+  }
+
   return (
     <header>
       <div className="logo">
         <img src={logo} alt="" />
+      </div>
+
+      <div onClick={setMenu} className="hamburguer">
+        <img src={menuHamburguer} alt="" />
       </div>
       <nav className="menu">
         <ul>
