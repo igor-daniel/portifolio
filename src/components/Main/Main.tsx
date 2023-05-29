cf49ffc1640fec00e59534c5d7dd099d4f0e0491
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-05-29 15:16:35 -0300

    main alterado

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -7,14 +7,10 @@ export const Main = () => {
       <main>
         <div className="limite conteudo">
           <section className="main-sec ">
-            <h1>Olá me chamo Igor Daniel e sou desenvolvedor front-end</h1>
-            <p>Estudante de Análise e desenvolvimento de Sistemas pela Estacio de Sá e, atualmente militar do exercito brasileiro.</p>
-            <ul className="links">
-              <li><a href="https://github.com/igor-daniel">GitHub</a></li>
-              <li><a href="https://www.linkedin.com/in/igor-daniel-8bbb74216/">Linkedln</a></li>
-              <li><a href="https://www.instagram.com/igordaniel5/">Instagram</a></li>
-              <li><a href="../Curriculo/curriculo.html">Currículo</a></li>
-            </ul>
+            <p className='cargo'>Web Developer</p>
+            <h1>Olá eu sou Igor Daniel <strong>.</strong></h1>
+            <p>Um desenvolvedor Web Frontend, apaixonado por <br /> tecnologia, especialista em criação de sites</p>
+            <a className='botao' href="#">Fale comigo</a>
           </section>
           <section className="image" data-tilt="">
             <img data-tilt src={programer} alt="Foto de Igor" />
