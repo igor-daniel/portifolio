f6dfe2db1c49bb9b89e03fa9ea061f08fa625122
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-06-04 19:42:48 -0300

    menu hamburguer adicionado

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import logo from './Foto-icon.svg'
-import menuHamburguer from './hambuguer.png'
+import menuHamburguer from './hambu.svg'
 import './header.css'
 export const Header = () => {
-  const setMenu = () => {
-    let menu = document.querySelector('nav');
-    menu?.classList.toggle("block")
+
+  const setClass = () => {
+    document.querySelector('.menu-dropdown')?.classList.toggle('active')
   }
+
 
   return (
     <header>
@@ -18,14 +19,27 @@ export const Header = () => {
       {/* <div onClick={setMenu} className="hamburguer">
         <img src={menuHamburguer} alt="" />
       </div> */}
+
       <nav className="menu">
-        <ul>
+        <div className="bt" onClick={setClass}>
+          <img className="hamburguer" src={menuHamburguer} alt="" />
+        </div>
+
+        <ul className="men">
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#skills">Habilidades</a></li>
           <li><a href="#projetos">Projetos</a></li>
           <li><a href="#contato" >Contato</a></li>
         </ul>
       </nav>
+      <div className="menu-dropdown">
+        <ul className="men">
+          <li><a href="#sobre">Sobre</a></li>
+          <li><a href="#skills">Habilidades</a></li>
+          <li><a href="#projetos">Projetos</a></li>
+          <li><a href="#contato" >Contato</a></li>
+        </ul>
+      </div>
     </header>
   )
 }
