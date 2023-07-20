3a88fff176d0fe667424b5e37d68247e0e4c1ea9
Igor Daniel da Silva <44319834+igor-daniel@users.noreply.github.com>
2023-07-20 18:13:55 -0300

    Link com menssagem para o whatsaap

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -10,7 +10,7 @@ export const Main = () => {
             <p className='cargo'>Web Developer</p>
             <h1>Olá eu sou Igor Daniel <strong>.</strong></h1>
             <p>Um desenvolvedor Web Frontend, apaixonado por tecnologia e especialista em criação de sites</p>
-            <a className='botao' href="https://www.linkedin.com/in/igor-daniel-8bbb74216/">Fale comigo</a>
+            <a className='botao' target='blank' href="https://wa.me/5521965961939?text=Olá%20Igor,%20tenho%20interesse%20em%20seus%20serciços">Fale comigo</a>
           </section>
           <section className="image" data-tilt="">
             <img data-tilt src={programer} alt="Foto de Igor" />
